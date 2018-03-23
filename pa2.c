595d369cc1f7fcc1549df6ba18b1e4b5a5a25b64
galatagrl <deryahancock79@gmail.com>
2018-03-23 11:00:40 -0400

    correct version of the loop! in device read

--- pa2.c
+++ pa2.c
@@ -119,9 +119,9 @@ static int dev_open(struct inode *inodep, struct file *filep)
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
 {
     int errorCount = 0;
-    int i=0;
+    int i= 0;
     int stringLen=len;
-    int off = *offset;
+    //int off = *offset;
 
     printk(KERN_INFO "\nPA2: READ Full string: %s\n", message);
 
@@ -131,12 +131,14 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
     // then reduce the read length to the maximum available
     // else use the requested read length
     //NOT SURE ABOUT THIS LINE
-    stringLen = BUFFER_LENGTH < len ? BUFFER_LENGTH : len;
-    //len = strlen(message) < len ? strlen(message) : len;
+   /* stringLen = BUFFER_LENGTH < len ? BUFFER_LENGTH : len;  */
+    len = strlen(message) < len ? strlen(message) : len;
     errorCount = copy_to_user(buffer, message, len);
      while (stringLen>0)
      {
-        buffer[i]= message[i+off];
+        message[i]= message[i+len];
+        i=i+1;
+
         stringLen--;
 
      }
